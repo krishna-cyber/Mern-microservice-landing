03e732513b2031d53f78609612332ccd7ea406f8
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2025-07-26 13:16:29 +0545

    feat: enhance UserForm component with Flowbite elements for improved UI and functionality

--- app/ordersSummary/components/userForm.tsx
+++ app/ordersSummary/components/userForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button } from "flowbite-react";
+import { Button, Label, TextInput, Textarea } from "flowbite-react";
 import { Card } from "flowbite-react";
 // import { Input } from "@/components/ui/input";
 // import { Label } from "@/components/ui/label";
@@ -36,112 +36,96 @@ const UserForm = () => {
   return (
     <section className=" container  mt-8 flex gap-4 justify-around mx-auto">
       <Card className=" w-[40%]">
-        {/* <CardHeader>
-          <CardTitle>Customer Details</CardTitle>
-        </CardHeader>
-        <CardContent className="grid gap-6">
-          <div className="grid gap-2">
-            <Label htmlFor="first-name">First name:</Label>
-            <Input
-              id="first-name"
-              value={customer?.firstName}
-              placeholder="first name"
-              disabled
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="last-name">Last name:</Label>
-            <Input
-              id="last-name"
-              value={customer?.lastName}
-              placeholder="last name"
-              disabled
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">E-mail:</Label>
-            <Input
-              id="email"
-              placeholder="email"
-              value={customer?.email}
-              disabled
-            />
-          </div>
-          <Card>
-            <CardHeader>
-              <CardTitle>Addresses:</CardTitle>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button variant="outline">+ Add address</Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-md">
-                  <DialogHeader>
-                    <DialogTitle>Add address</DialogTitle>
-                    <DialogDescription>
-                      Delivery address or reference
-                    </DialogDescription>
-                  </DialogHeader>
-                  <div className="flex items-center gap-2">
-                    <div className="grid flex-1 gap-2">
-                      <Label htmlFor="link" className="sr-only">
-                        Link
-                      </Label>
-                      <Input
-                        id="link"
-                        placeholder="Delivery address "
-                        readOnly
-                      />
-                    </div>
+        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+          Customer Details
+        </h5>
+        <div className="grid gap-2">
+          <Label htmlFor="first-name">First name:</Label>
+
+          <TextInput id="first-name" value={customer?.firstName} disabled />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="last-name">Last name:</Label>
+
+          <TextInput id="last-name" value={customer?.lastName} disabled />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="email">E-mail:</Label>
+
+          <TextInput id="email" value={customer?.email} disabled />
+        </div>
+        <Card>
+          {/* <CardHeader>
+            <CardTitle>Addresses:</CardTitle>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="outline">+ Add address</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                  <DialogTitle>Add address</DialogTitle>
+                  <DialogDescription>
+                    Delivery address or reference
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="flex items-center gap-2">
+                  <div className="grid flex-1 gap-2">
+                    <Label htmlFor="link" className="sr-only">
+                      Link
+                    </Label>
+                    <Input id="link" placeholder="Delivery address " readOnly />
                   </div>
-                  <DialogFooter className="sm:justify-start">
-                    <DialogClose asChild>
-                      <Button type="button" variant="secondary">
-                        Close
-                      </Button>
-                    </DialogClose>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-            </CardHeader>
-            <RadioGroup
-              defaultValue="card"
-              className="grid grid-cols-3 gap-4"
-            ></RadioGroup>
-          </Card>
-          <RadioGroup defaultValue="card" className="grid grid-cols-3 gap-4">
-            <div>
-              <RadioGroupItem value="card" id="card" className="peer sr-only" />
-              <Label
-                htmlFor="card"
-                className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-              >
-                <CreditCard />
-                Card
-              </Label>
-            </div>
-            <div>
-              <RadioGroupItem
-                value="paypal"
-                id="paypal"
-                className="peer sr-only"
-              />
-              <Label
-                htmlFor="paypal"
-                className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-              >
-                <Banknote />
-                Cash
-              </Label>
-            </div>
-          </RadioGroup>
-
-         
-
-          <div className="grid w-full gap-3">
-            <Label htmlFor="message">Your message</Label>
-            <Textarea placeholder="Type your message here." id="message" />
+                </div>
+                <DialogFooter className="sm:justify-start">
+                  <DialogClose asChild>
+                    <Button type="button" variant="secondary">
+                      Close
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </CardHeader>
+          <RadioGroup
+            defaultValue="card"
+            className="grid grid-cols-3 gap-4"
+          ></RadioGroup> */}
+        </Card>
+        {/* <RadioGroup defaultValue="card" className="grid grid-cols-3 gap-4">
+          <div>
+            <RadioGroupItem value="card" id="card" className="peer sr-only" />
+            <Label
+              htmlFor="card"
+              className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+            >
+              <CreditCard />
+              Card
+            </Label>
           </div>
-        </CardContent> */}
+          <div>
+            <RadioGroupItem
+              value="paypal"
+              id="paypal"
+              className="peer sr-only"
+            />
+            <Label
+              htmlFor="paypal"
+              className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+            >
+              <Banknote />
+              Cash
+            </Label>
+          </div>
+        </RadioGroup> */}
+
+        <div className="grid w-full gap-3">
+          <Label htmlFor="message">Your message or instruction:</Label>
+          <Textarea
+            placeholder="You can leave a note or instruction for order."
+            id="message"
+            rows={4}
+          />
+        </div>
       </Card>
       <Card className=" w-[40%] h-fit">
         {/* <CardHeader>
